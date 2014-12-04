eaf6dfee9b96f2eb0552507d04991ee2f439073b
yamorn <yamorn@outlook.com>
2014-12-04 16:00:46 +0800

    hdfs-zip

--- src/main/java/com/sun/hadoopdemo/zip/ZipMapReduceDemo.java
+++ src/main/java/com/sun/hadoopdemo/zip/ZipMapReduceDemo.java
@@ -1,6 +1,8 @@
 package com.sun.hadoopdemo.zip;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -8,17 +10,27 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by louis on 2014/12/3.
  */
 public class ZipMapReduceDemo {
     public static class ZipMapper extends Mapper<ZipWrapEntry, ZipContent, Text, Text> {
+        private static Path rootPath;
+        private static FileSystem fileSystem;
+        public void setup(Context context) throws IOException {
+            Configuration conf = context.getConfiguration();
+            fileSystem = FileSystem.get(conf);
+            rootPath = new Path(conf.get("convert.output"));
+        }
         @Override
         protected void map(ZipWrapEntry key, ZipContent value, Context context) throws IOException, InterruptedException {
-
             String fileName = key.getZipEntry().getName();
             long length = value.getContent().length;
+            OutputStream outputStream = fileSystem.create(new Path(rootPath, fileName));
+            IOUtils.write(value.getContent(), outputStream);
+            IOUtils.closeQuietly(outputStream);
             context.write(new Text(fileName), new Text(length + ""));
         }
     }
@@ -27,6 +39,8 @@ public class ZipMapReduceDemo {
         Configuration conf = new Configuration();
         conf.setBoolean("fs.hdfs.impl.disable.cache", true);
         conf.set("fs.default.name", "hdfs://192.168.0.14:9000");
+        conf.set("mapred.job.tracker", "192.168.0.14:9001");
+        conf.set("convert.output","/user/hadoop/fileoutput");
         String otherArgs[] = new String[]{
                 "/user/hadoop/pic.zip",
                 "/user/hadoop/output"
